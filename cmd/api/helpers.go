91f8c04ae6566f9c9bb94d0af7bf7a8ea9506b31
Amy Dang <kirontoo@gmail.com>
2024-04-29 13:38:22 -0700

    3.5 Advanced JSON Customization

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
-  "encoding/json"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -20,8 +20,10 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-	js, err := json.Marshal(data)
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
