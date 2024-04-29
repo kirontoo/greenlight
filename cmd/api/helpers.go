3ec2caf813a5362a6ed1cd58f0201376ed223a1e
Amy Dang <kirontoo@gmail.com>
2024-04-29 12:41:06 -0700

    3.2 encode json

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
+  "encoding/json"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -17,4 +18,23 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
