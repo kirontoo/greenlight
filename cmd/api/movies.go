3ec1c3ec20ccb38a38bbde3052fa7e9011e63981
Amy Dang <kirontoo@gmail.com>
2024-04-29 13:08:01 -0700

    3.3 Encode structs

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+	"github.com/kirontoo/greenlight/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +18,17 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprint(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID: id,
+		CreatedAt: time.Now(),
+		Runtime: 102,
+		Genres: []string{"drama", "romance", "war"},
+		Version: 1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Print(err)
+    http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
