3ca4139bdb9d8b728f26b3574742367cfd98a17f
Amy Dang <ndangamy@gmail.com>
2024-06-26 21:12:17 -0700

    14.5 Graceful shutdown of background tasks

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/kirontoo/greenlight/internal/data"
@@ -45,6 +46,7 @@ type application struct {
 	logger *jsonlog.Logger
 	models data.Models
 	mailer mailer.Mailer
+	wg sync.WaitGroup
 }
 
 func main() {
