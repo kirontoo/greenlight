11da243c954ee1242bf249f007e97f8c6f6985d5
Amy Dang <ndangamy@gmail.com>
2024-07-05 12:42:02 -0700

    16.2 Generate User Auth Tokens

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -13,14 +13,18 @@ import (
 
 const (
 	ScopeActivation = "activation"
+	ScopeAuthentication = "authentication"
 )
 
+
+// only the Plaintext and Expiry field will be included when encoding a Token
+// all other fields will be omitted
 type Token struct {
-	Plaintext string
-	Hash      []byte
-	UserID    int64
-	Expiry    time.Time
-	Scope     string
+	Plaintext string `json:"token"`
+	Hash      []byte `json:"-"`
+	UserID    int64 `json:"-"`
+	Expiry    time.Time `json:"expiry"`
+	Scope     string `json:"-"`
 }
 
 func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
