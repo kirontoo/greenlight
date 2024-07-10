aefce3c50b456a436ae7511baf163d5095bd06e8
Amy Dang <ndangamy@gmail.com>
2024-07-10 10:57:57 -0700

    17.3 Create Permission Model

--- internal/data/model.go
+++ internal/data/model.go
@@ -12,14 +12,16 @@ var (
 
 type Models struct {
 	Movies Movies
-	Users UserModel
+	Permissions PermissionModel
 	Tokens TokenModel
+	Users UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
-		Users: UserModel{DB: db},
+		Permissions: PermissionModel{DB: db},
 		Tokens: TokenModel{DB: db},
+		Users: UserModel{DB: db},
 	}
 }
