1e5944f7eee598f4f52e8f7d9d299c310df79024
Amy Dang <ndangamy@gmail.com>
2024-07-23 11:07:06 -0700

    20.4 Module vendors & prep for deployment

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -13,6 +12,7 @@ import (
 
 	"github.com/kirontoo/greenlight/internal/data"
 	"github.com/kirontoo/greenlight/internal/validator"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -76,36 +76,25 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
+			// Use the realip.FromRequest() function to get the client's real IP address.
+			ip := realip.FromRequest(r)
 
-			// Extract the client's IP address from the request.
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
-
-			// Lock the mutex to prevent this code from being executed concurrently.
 			mu.Lock()
 
-			// Check to see if the IP address already exists in the map. If it doesn't, then
-			// initialize a new rate limiter and add the IP address and limiter to the map.
 			if _, found := clients[ip]; !found {
-				clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+				}
 			}
 
-			// Call the Allow() method on the rate limiter for the current IP address. If
-			// the request isn't allowed, unlock the mutex and send a 429 Too Many Requests
-			// response
+			clients[ip].lastSeen = time.Now()
+
 			if !clients[ip].limiter.Allow() {
 				mu.Unlock()
 				app.rateLimitExceededResponse(w, r)
 				return
 			}
 
-			// Very importantly, unlock the mutex before calling the next handler in the
-			// chain. Notice that we DON'T use defer to unlock the mutex, as that would mean
-			// that the mutex isn't unlocked until all the handlers downstream of this
-			// middleware have also returned.
 			mu.Unlock()
 		}
 
